72488888a97f605db4998194b61e5d2dbd061e53
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-07-23 00:58:19 -0400

    added comment form

--- blog-app/src/pages/ArticlePage.js
+++ blog-app/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import articles from './article-content';
 import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -47,6 +48,10 @@ const ArticlePage = () => {
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
+            <AddCommentForm 
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}
+            />
             <CommentsList comments={articleInfo.comments} />
         </>
     );
