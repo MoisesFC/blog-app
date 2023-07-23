105e2299966f3c1758f6c413639174389f3d5072
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-07-22 23:50:59 -0400

    some changes

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -3,17 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 import NotFoundPage from './pages/NotFoundPage';
-
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
-        <div id='page-body'>
+        <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
