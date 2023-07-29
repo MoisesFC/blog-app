ed1ff9be3a2d3a8dc8c378d7d82efb4ca5e0dcde
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-07-29 04:35:23 -0400

    fixed navbar responsiveness

--- blog-app/src/NavBar.js
+++ blog-app/src/NavBar.js
@@ -1,37 +1,45 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getAuth, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import useUser from './hooks/useUser';
 
 const NavBar = () => {
+  const { user } = useUser();
+  const navigate = useNavigate();
 
-    const { user } = useUser();
-    const navigate = useNavigate();
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/articles">Articles</Link>
-                </li>
-            </ul>
-            <div className='nav-right'>
-                {user
-                    ? <button onClick={() => {
-                        signOut(getAuth())
-                    }}>Log Out</button>
-                    : <button onClick={() => {
-                        navigate('/login')
-                    }}>Log In</button>}
-            </div>
-        </nav>
-    );
-}
+  const toggleMobileMenu = () => {
+    setShowMobileMenu((prevState) => !prevState);
+  };
+
+  return (
+    <nav>
+      <div className='nav-right'>
+        {user ? (
+          <button onClick={() => signOut(getAuth())}>Log Out</button>
+        ) : (
+          <button onClick={() => navigate('/login')}>Log In</button>
+        )}
+      </div>
+
+      <button className='mobile-menu-button' onClick={toggleMobileMenu}>
+        ☰
+      </button>
+      <ul className={`nav-links ${showMobileMenu ? 'show-mobile-menu' : ''}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
